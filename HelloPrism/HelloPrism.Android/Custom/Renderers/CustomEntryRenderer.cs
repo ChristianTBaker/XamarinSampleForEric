84b61047b6a90a1fd9698ec1550ebfd73922aaf8
Christian Baker <Christian.Baker@echo.com>
2020-10-22 00:35:11 -0500

    Added custom entry renderers on both platforms

---
+++ HelloPrism/HelloPrism.Android/Custom/Renderers/CustomEntryRenderer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel;
+using Android.Content;
+using Android.Content.Res;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Text;
+using Android.Widget;
+using HelloPrism.Controls;
+using HelloPrism.Droid.Custom.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace HelloPrism.Droid.Custom.Renderers
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.SetRawInputType(InputTypes.TextVariationWebEmailAddress);
+                Control.SetBackgroundColor(global::Android.Graphics.Color.ParseColor("#F5F5F5"));
+                Control.SetHintTextColor(ColorStateList.ValueOf(global::Android.Graphics.Color.Transparent));
+                Control.SetCursorVisible(true);
+
+                if (Build.VERSION.SdkInt <= BuildVersionCodes.P)
+                {
+                    IntPtr IntPtrtextViewClass = JNIEnv.FindClass(typeof(TextView));
+                    IntPtr mCursorDrawableResProperty = JNIEnv.GetFieldID(IntPtrtextViewClass, "mCursorDrawableRes", "I");
+
+                    JNIEnv.SetField(Control.Handle, mCursorDrawableResProperty, Resource.Drawable.custom_cursor);
+                }
+                else
+                {
+                    Control.SetTextCursorDrawable(Resource.Drawable.custom_cursor);
+                }
+
+
+            }
+
+        }
+    }
+}
---
+++ HelloPrism/HelloPrism.iOS/Custom/Renderers/CustomEntryRenderer.cs
@@ -0,0 +1,26 @@
+﻿using HelloPrism.Controls;
+using HelloPrism.iOS.Custom.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using EntryRenderer = Xamarin.Forms.Platform.iOS.EntryRenderer;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace HelloPrism.iOS.Custom.Renderers
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.BackgroundColor = UIColor.Clear;
+                Control.BorderStyle = UITextBorderStyle.None;
+                Control.KeyboardType = UIKeyboardType.EmailAddress;
+                Control.TintColor = UIColor.Black;
+            }
+        }
+    }
+}
