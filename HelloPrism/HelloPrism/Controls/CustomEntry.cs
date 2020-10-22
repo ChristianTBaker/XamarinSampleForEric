ed659e9f92254c910571637c5d53cd8165be3282
Christian Baker <Christian.Baker@echo.com>
2020-10-21 23:56:00 -0500

    Submit control template, navigation, setup for custom entry renderer

---
+++ HelloPrism/HelloPrism/Controls/CustomEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace HelloPrism.Controls
+{
+    public class CustomEntry : Entry
+    {
+        public CustomEntry()
+        {
+        }
+    }
+}
--- HelloPrism/HelloPrism/ViewModels/MainViewModel.cs
+++ HelloPrism/HelloPrism/ViewModels/MainViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,24 +9,69 @@ namespace HelloPrism.ViewModels
 {
     public class MainViewModel : BindableBase
     {
-        public MainViewModel()
+        private INavigationService _navigationService;
+
+        public DelegateCommand SubmitCommand { get; private set; }
+
+        public string Title { get; private set; }
+        public string WelcomeMessage { get; private set; }
+        public string FirstNameLabel { get; private set; }
+        public string LastNameLabel { get; private set; }
+        public string EmailLabel { get; private set; }
+        public string SubmitButtonLabel { get; private set; }
+
+        public MainViewModel(INavigationService navigationService)
         {
+            this._navigationService = navigationService;
+
             this.Title = "Main Page";
             this.WelcomeMessage = "Welcome to Xamarin Sample!";
+            this.FirstNameLabel = "First Name";
+            this.LastNameLabel = "Last Name";
+            this.EmailLabel = "email";
+            this.SubmitButtonLabel = "View Service Events";
+
+            this.SubmitCommand = new DelegateCommand(Submit, CanSubmit);
         }
 
-        private string title;
-        public string Title
+        private void Submit()
         {
-            get => this.title;
-            set => SetProperty(ref this.title, value);
+            _navigationService.NavigateAsync("Events");
         }
 
-        private string welcomeMessage;
-        public string WelcomeMessage
+        private bool CanSubmit()
         {
-            get => this.welcomeMessage;
-            set => SetProperty(ref this.welcomeMessage, value);
+            //if (!string.IsNullOrEmpty(this.FirstNameEntry) &&
+            //    !string.IsNullOrEmpty(this.LastNameEntry) &&
+            //    !string.IsNullOrEmpty(this.EmailEntry))
+            // {
+            //     return true;
+            // }
+
+            // return false;
+
+            return true;
+        }
+
+        private string _firstNameEntry;
+        public string FirstNameEntry
+        {
+            get => this._firstNameEntry;
+            set => SetProperty(ref this._firstNameEntry, value);
+        }
+
+        private string _lastNameEntry;
+        public string LastNameEntry
+        {
+            get => this._lastNameEntry;
+            set => SetProperty(ref this._lastNameEntry, value);
+        }
+
+        private string _emailEntry;
+        public string EmailEntry
+        {
+            get => this._emailEntry;
+            set => SetProperty(ref this._emailEntry, value);
         }
     }
 }
